112bbedecb7b77d1f35e76d8c7e146975a3597d3
shuvakant patra <scientefic2612@gmail.com>
2025-07-20 14:01:35 +0530

    commit

--- python/calender.py
+++ python/calender.py
@@ -3,9 +3,6 @@ import calendar
 year = int(input("Enter the year : "))
 month = int(input("Enter the month : "))
 
-if month == :
-    cal = calendar.calendar(year)
-else:
-    cal = calendar.month(year,month)
+cal = calendar.month(year,month)
 
 print(cal)
